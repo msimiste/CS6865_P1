3e725c26cd6f16939a5ae35dd37857c337f4017b
Michael Simister <msimiste@gc112m22.cs.unb.ca>
2020-11-29 17:03:14 -0400

    Cubic Implementation, also added blended folder/files

--- Cubic/Cubic.tcl
+++ Cubic/Cubic.tcl
@@ -29,30 +29,24 @@ proc finish {} {
 }
 
 proc record {} {
-        global sink0 sink1 sink2 sink3 f0 f1 f2 f3
-	#Get an instance of the simulator
-	set ns [Simulator instance]
+        global ns tcp0 tcp1 tcp2 tcp3 f0 f1 f2 f3
+	
 	#Set the time after which the procedure should be called again
-        set time 1.0
+        set time 0.1
 	#How many bytes have been received by the traffic sinks?
-        set bw0 [$sink0 set bytes_]
-	set bw1	[$sink1 set bytes_]
-	set bw2 [$sink2 set bytes_]
-	set bw3 [$sink3 set bytes_]
+        set cwnd0 [$tcp0 set cwnd_]
+	set cwnd1 [$tcp1 set cwnd_]
+	set cwnd2 [$tcp2 set cwnd_]
+	set cwnd3 [$tcp3 set cwnd_]
 		
 	#Get the current time
         set now [$ns now]
 	#Calculate the bandwidth (in MBit/s) and write it to the files
-        puts $f0 "$now [expr $bw0/$time*8/1000000]"
-	puts $f1 "$now [expr $bw1/$time*8/1000000]"
-	puts $f2 "$now [expr $bw2/$time*8/1000000]"
-	puts $f3 "$now [expr $bw3/$time*8/1000000]"
+        puts $f0 "$now $cwnd0"
+	puts $f1 "$now $cwnd1"
+	puts $f2 "$now $cwnd2"
+	puts $f3 "$now $cwnd3"
 	
-	#Reset the bytes_ values on the traffic sinks
-        $sink0 set bytes_ 0
-	$sink1 set bytes_ 0
-	$sink2 set bytes_ 0
-	$sink3 set bytes_ 0
 	#Re-schedule the procedure
         $ns at [expr $now+$time] "record"
 }
@@ -71,15 +65,15 @@ set n9 [$ns node]
 
 
 #Create a duplex link between the nodes
-$ns duplex-link $n0 $n2 1Mb 10ms DropTail
-$ns duplex-link $n1 $n2 1Mb 10ms DropTail
-$ns duplex-link $n2 $n3 1Mb 10ms DropTail
-$ns duplex-link $n3 $n4 1Mb 10ms DropTail
-$ns duplex-link $n3 $n5 1Mb 10ms DropTail
-$ns duplex-link $n6 $n2 1Mb 10ms DropTail
-$ns duplex-link $n3 $n7 1Mb 10ms DropTail
-$ns duplex-link $n8 $n2 1Mb 10ms DropTail
-$ns duplex-link $n3 $n9 1Mb 10ms DropTail
+$ns duplex-link $n0 $n2 10Mb 2ms DropTail
+$ns duplex-link $n1 $n2 10Mb 2ms DropTail
+$ns duplex-link $n2 $n3 100Mb 2ms DropTail
+$ns duplex-link $n3 $n4 10Mb 2ms DropTail
+$ns duplex-link $n3 $n5 10Mb 2ms DropTail
+$ns duplex-link $n6 $n2 10Mb 2ms DropTail
+$ns duplex-link $n3 $n7 10Mb 2ms DropTail
+$ns duplex-link $n8 $n2 10Mb 2ms DropTail
+$ns duplex-link $n3 $n9 10Mb 2ms DropTail
 
 
 $ns duplex-link-op $n2 $n0 orient left-up
@@ -93,30 +87,30 @@ $ns duplex-link-op $n8 $n2 orient down
 $ns duplex-link-op $n3 $n9 orient down
 
 #Create a TCP agent and attach it to node n0
-set tcp0 [new Agent/TCP/Vegas]
+set tcp0 [new Agent/TCP/Linux]
 $tcp0 set packetSize_ 1500
-$tcp0 set window_ 85
+$tcp0 set window_ 128
 $ns attach-agent $n0 $tcp0
 $tcp0 set class_ 2
 
 #Create a TCP agent and attach it to node n1
-set tcp1  [new Agent/TCP/Vegas]
+set tcp1  [new Agent/TCP/Linux]
 $tcp1 set packetSize_ 1500
-$tcp1 set window_ 85
+$tcp1 set window_ 128
 $ns attach-agent $n1 $tcp1
 $tcp1 set class_ 1
 
 #Create a TCP agent and attach it to node n0
-set tcp2 [new Agent/TCP/Vegas]
+set tcp2 [new Agent/TCP/Linux]
 $tcp2 set packetSize_ 1500
-$tcp2 set window_ 85
+$tcp2 set window_ 128
 $ns attach-agent $n6 $tcp2
 $tcp2 set class_ 3
 
 #Create a TCP agent and attach it to node n0
-set tcp3 [new Agent/TCP/Vegas]
+set tcp3 [new Agent/TCP/Linux]
 $tcp3 set packetSize_ 1500
-$tcp3 set window_ 85
+$tcp3 set window_ 128
 $ns attach-agent $n8 $tcp3
 $tcp3 set class_ 4
 
@@ -132,16 +126,20 @@ $ftp2 attach-agent $tcp2
 set ftp3 [new Application/FTP]
 $ftp3 attach-agent $tcp3
 
-set sink0 [new Agent/TCPSink]
+set sink0 [new Agent/TCPSink/Sack1]
+$sink0 set ts_echo_rfc1323_ true
 $ns attach-agent $n4 $sink0
 
-set sink1 [new Agent/TCPSink]
+set sink1 [new Agent/TCPSink/Sack1]
+$sink1 set ts_echo_rfc1323_ true
 $ns attach-agent $n5 $sink1
 
-set sink2 [new Agent/TCPSink]
+set sink2 [new Agent/TCPSink/Sack1]
+$sink2 set ts_echo_rfc1323_ true
 $ns attach-agent $n7 $sink2
 
-set sink3 [new Agent/TCPSink]
+set sink3 [new Agent/TCPSink/Sack1]
+$sink3 set ts_echo_rfc1323_ true
 $ns attach-agent $n9 $sink3
 
 $ns connect $tcp0 $sink0
